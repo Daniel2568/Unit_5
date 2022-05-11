ed2efc632321653a1e57b28f2eeeb19081451bf3
Daniel Worth <dworth25@students.bmchs.org>
2022-05-11 09:03:37 -0500

    Done

--- Lab 5.02.py
+++ Lab 5.02.py
@@ -48,85 +48,45 @@ example_paragraph_lower_no_punctuation = example_paragraph_lower.replace(".", ""
 #convert paragraph into a list of individual strings
 example_word_list = example_paragraph_lower_no_punctuation.split(" ")
 '''
-my_paragraph = "Dogs can be very good pets. In my opinion dogs are superior to cats. The reason being because dogs do more than just sit around.\nI would prefer to have a pet that does more than just be there for emotional support. Dogs can help do things for you and be a medical aid if you need them to be.\nDogs also have the energy to play more than cats which makes it better to have dogs around than cats. Its a little biased but I like dogs more than cats."
+my_paragraph = '''Dogs can be very good pets. In my opinion dogs are superior to cats. 
+The reason being because dogs do more than just sit around.
+I would prefer to have a pet that does more than just be there for emotional support.
+Dogs can help do things for you and be a medical aid if you need them to be.
+Dogs also have the energy to play more than cats which makes it better to have dogs around than cats.
+Its a little biased but I like dogs more than cats.'''
 
-my_paragraph_dict = {
-'dogs': '7',
-'can': '2',
-'be': '4',
-'being':'1',
-'very': '1',
-'good': '1',
-'pets': '1',
-'in': '1',
-'my': '1',
-'opinion': '1',
-'are': '1',
-'superior': '1',
-'to': '5',
-'cats': '4',
-'the': '1',
-'reason': '1',
-'because': '1',
-'do': '2',
-'more': '4',
-'than': '5',
-'just': '2',
-'sit': '1',
-'around': '1',
-'I': '2',
-'would': '1',
-'prefer': '1',
-'have': '3',
-'a': '2',
-'pet': '1',
-'that': '1',
-'does': '1',
-'for': '2',
-'emotional': '1',
-'support': '1',
-'help': '1',
-'things': '1',
-'you': '1',
-'and': '1',
-'medical': '1',
-'aid': '1',
-'if': '1',
-'need': '1',
-'them': '1',
-'also': '1',
-'have': '2',
-'energy': '1',
-'play': '1',
-'makes': '1',
-'it': '1',
-'its': '1',
-'which': '1',
-'better': '1',
-'little': '1',
-'biased': '1',
-'but': '1',
-'like': '1',
-}
-
-#find frequency of all words in paragraph
-print(f"{my_paragraph}")
-print()
-word_choice = input("What word would you like to know the frequency of?>")
-def frequency(word):
-    if word in my_paragraph_dict:
-        print(f"The word {word_choice} appears {my_paragraph_dict[word_choice]} time(s) in the paragraph.")
-    else:
-        print(f"The word {word_choice} is not used in this paragraph.")
-
-frequency(word_choice)
+#print my_paragraph
+print(my_paragraph)
 
 #make all letters lowercase
 my_paragraph_lower = my_paragraph.lower()
 
 #remove all periods
 my_paragraph_lower_no_punctuation = my_paragraph_lower.replace(".", "")
+my_paragraph_lower_no_punctuation = my_paragraph_lower_no_punctuation.replace("\n", "")
 
 
 #convert paragraph into a list of individual strings
 word_list = my_paragraph_lower_no_punctuation.split(" ")
+
+#my paragraph dict
+my_paragraph_dict = {}
+
+#populate word dict
+for word in word_list:
+    if word in my_paragraph_dict:
+        my_paragraph_dict[word] += 1
+    else:
+        my_paragraph_dict[word] = 1
+
+
+while True:
+
+    user_choice = input("What word would you like to know the frequency of?>")
+
+    if word in my_paragraph_dict:
+        print(f"The word '{user_choice}' appears {my_paragraph_dict[user_choice]} time(s) in the paragraph.")
+    elif user_choice == 'q':
+        break
+    else:
+        print(f"The word '{user_choice}' is not used in this paragraph.")
